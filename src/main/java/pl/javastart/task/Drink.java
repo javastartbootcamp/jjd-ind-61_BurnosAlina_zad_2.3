bb2846990ebc529a63e2a9fbe1f27794ffc502a5
Alina Burnos <burnosalina@gmail.com>
2023-01-05 14:35:55 +0100

    Drink i jego sklad

---
+++ src/main/java/pl/javastart/task/Drink.java
@@ -0,0 +1,11 @@
+package pl.javastart.task;
+
+public class Drink {
+    String name;
+    double price;
+    boolean alcoholic;
+    Ingredients ingredient1;
+    Ingredients ingredient2;
+    Ingredients ingredient3;
+
+}
